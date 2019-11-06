b39f421dce963a354fe26d3736ebbddb3a6a9ad4
LoboMetalurgico <hugolino1000@gmail.com>
2019-11-05 22:51:30 -0300

    Initial Commit

---
+++ src/main/java/tk/lobometalurgico/AllonsyMod.java
@@ -0,0 +1,31 @@
+package tk.lobometalurgico;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = AllonsyMod.MODID, name = AllonsyMod.NAME, version = AllonsyMod.VERSION)
+public class AllonsyMod
+{
+    public static final String MODID = "alonsymod";
+    public static final String NAME = "Allons-y Mod";
+    public static final String VERSION = "@VERSION@";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
