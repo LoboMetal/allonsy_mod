884454e509adbccfffbc851239ee542d38848239
LoboMetalurgico <hugolino1000@gmail.com>
2019-11-07 23:35:49 -0300

    Atualizando o arquivo Names para Reference

--- src/main/java/tk/lobometalurgico/util/Names.java
+++
@@ -1,8 +0,0 @@
-package tk.lobometalurgico.util;
-
-public class Names
-{
-    public static final String MODID = "alonsymod";
-    public static final String NAME = "Allons-y Mod";
-    public static final String VERSION = "@VERSION@";
-}
---
+++ src/main/java/tk/lobometalurgico/util/Reference.java
@@ -0,0 +1,10 @@
+package tk.lobometalurgico.util;
+
+public class Reference {
+    public static final String MOD_ID = "alonsymod";
+    public static final String NAME = "Allons-y Mod";
+    public static final String VERSION = "@VERSION@";
+    public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+    public static final String CLIENT_PROXY_CLASS = "tk.lobometalurgico.proxy.ClientProxy";
+    public static final String COMMON_PROXY_CLASS = "tk.lobometalurgico.proxy.CommomProxy";
+}
