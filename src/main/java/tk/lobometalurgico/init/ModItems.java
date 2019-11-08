3283cc32eb64214f259b7d7cefae586e134fdbff
LoboMetalurgico <hugolino1000@gmail.com>
2019-11-08 13:53:10 -0300

    Alterando a referência ao item "ruby" (FAILED)

--- src/main/java/tk/lobometalurgico/init/ModItems.java
+++ src/main/java/tk/lobometalurgico/init/ModItems.java
@@ -9,5 +9,5 @@ import java.util.List;
 public class ModItems {
     public static final List<Item> ITEMS = new ArrayList<Item>();
 
-    public static final Item RUBY = new ItemBase("ruby");
+    public static final Item ruby = new ItemBase("ruby");
 }
